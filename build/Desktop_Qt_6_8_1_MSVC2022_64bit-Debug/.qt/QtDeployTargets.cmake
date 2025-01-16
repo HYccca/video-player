14c150cbbb7e1647d5eb1cbbe6557e4b4181313f
HYccca <3021509322@qq.com>
2025-01-16 22:11:50 +0800

    Delete build directory

--- build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled8_FILE E:/untitled8/build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/untitled8.exe)
-set(__QT_DEPLOY_TARGET_untitled8_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled8_RUNTIME_DLLS F:/qt6/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;F:/qt6/6.8.1/msvc2022_64/bin/Qt6Guid.dll;F:/qt6/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
